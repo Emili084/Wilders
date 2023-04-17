2dc5c66010e740cc22d7508539a588cd4dba4315
Emili084 <128594426+Emili084@users.noreply.github.com>
2023-04-17 11:14:55 +0200

    Json et js

---
+++ index.js
@@ -0,0 +1,14 @@
+import chalk from 'chalk';
+
+console.log(chalk.blue('Richard'));
+console.log(chalk.red('Marcos'));
+console.log(chalk.green('Mathieu'));
+console.log(chalk.yellow('Damien'));
+
+
+
+const name = 'John Doe';
+const age = 26;
+const passions = ['Ski', 'Hiking', 'Cinema'];
+
+console.log(name, age, passions);
